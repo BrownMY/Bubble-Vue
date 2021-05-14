0eedf542accb8978615b5198fb813ca247b55193
mbrown <tugtuj@gmail.com>
2021-05-14 18:58:24 -0400

    Add Task form add

--- bubble-vue/src/App.vue
+++ bubble-vue/src/App.vue
@@ -3,6 +3,7 @@
    <h1>Bubble</h1> 
    <p>Hydration Tracker</p>
    <Header title="Task Tracker"/>
+   <AddTask @add-task="addTask"/>
    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
   
@@ -12,12 +13,14 @@
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask
   },
 
   data() {
@@ -26,6 +29,10 @@ export default {
     }
   },
   methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task]
+    },
+
     deleteTask(id) {
       if(confirm('Are you sure?'))
       this.tasks = this.tasks.filter((task) => task.id !== id)
